5c3f2991c7525897cbded38f62b14a7bd75df941
Robert Altner <robert.altner11@gmail.com>
2022-12-06 16:22:55 +0100

    begin work on wrapper

---
+++ control/src/wrapper/b15f.cpp
@@ -0,0 +1,6 @@
+#include "b15f.h"
+#include "../drv/b15f.h"
+
+b15f_t* get_instance(void) {
+    
+}
---
+++ control/src/wrapper/b15f.h
@@ -0,0 +1,9 @@
+#ifndef _B15F_H_
+#define _B15F_H_
+
+struct b15f;
+typedef struct b15f b15f_t;
+
+b15f_t* get_instance(void); 
+
+#endif // _B15F_H_
