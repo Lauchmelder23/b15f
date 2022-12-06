ac85394396b3757b39c81e6010b1d0b76442f74c
Robert <robert.altner11@gmail.com>
2022-12-06 20:45:35 +0100

    update example for change in function signature

--- control/examples/wrapper/main.c
+++ control/examples/wrapper/main.c
@@ -1,7 +1,14 @@
 #include "b15f/wrapper/b15f.h"
+#include <stdio.h>
 
 int main() {
-	b15f_t* instance = get_instance();
+	char buf[512];
+	b15f_t* instance = get_instance(buf, sizeof(buf));
+
+	if (instance == NULL) {
+		fprintf(stderr, "%s", buf);
+		return -1;
+	}
 
 	return 0;
 }
